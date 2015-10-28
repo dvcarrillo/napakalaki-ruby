70ceff3cfb60a940d4b96ed2f1454f546af79506
David Vargas <dvcarrillo@correo.ugr.es>
2015-10-28 18:26:33 +0100

    All the monsters now are on PruebaNapakalaki class
    We have added the remaining monsters on the main class

--- lib/PruebaNapakalaki.rb
+++ lib/PruebaNapakalaki.rb
@@ -15,30 +15,89 @@ require './monster.rb'
 require './treasure_kind.rb'
 
 ################################################################################
-# TEST OF THE CREATED CLASSES
+# TESTS OF THE CREATED CLASSES
 
 # Class Prize
-#level = 5
-#treasures = 2
+# level = 5
+# treasures = 2
 
-#prz = Prize.new(level,treasures)
+# prz = Prize.new(level,treasures)
 
-#puts "Test de Prize"
-#puts prz.level
-#puts prz.treasures
+# puts "Test de Prize"
+# puts prz.level
+# puts prz.treasures
 
 # Class Bad Consequence
-#puts "\nTest de Bad Consequence"
+# puts "\nTest de Bad Consequence"
 
-#badcon = BadConsequence.new("soy malo", 12, 2, 0, 0, 0, false)
+# badcon = BadConsequence.new("soy malo", 12, 2, 0, 0, 0, false)
 
-#puts badcon.to_s
+# puts badcon.to_s
 
 ################################################################################
 # DEFINITION OF THE MONSTERS
 
 # Array which will store the monsters
 monsters = Array.new
+
+# 3 BYAKHEES DE BONANZA
+prize = Prize.new(2, 1)
+bad_consequence = BadConsequence.new_level_specific_treasures('Pierdes tu
+armadura visible y otra oculta',0, [TreasureKind::ARMOR], [TreasureKind::ARMOR])
+monsters << Monster.new('3 Byakhees de bonanza', 8, bad_consequence, prize)
+
+# CHIBITHULHU
+prize = Prize.new(1, 1)
+bad_consequence = BadConsequence.new_level_specific_treasures('Embobados con el
+lindo primigenio te descartas de tu casco visible', 0, [TreasureKind::HELMET], 
+Array.new )
+monsters << Monster.new('Chibithulhu', 2, bad_consequence, prize)
+
+# EL SOPOR DE DUNWICH
+prize = Prize.new(1, 1)
+bad_consequence = BadConsequence.new_level_specific_treasures('El primordial 
+bostezo contagioso. Pierdes el calzado visible', 0, [TreasureKind::SHOES], 
+Array.new)
+monsters << Monster.new('Chibithulhu', 2, bad_consequence, prize)
+
+# ÁNGELES DE LA NOCHE IBICENCA
+prize = Prize.new(4, 1)
+bad_consequence = BadConsequence.new_level_specific_treasures('Te atrapan para 
+llevarte de fiesta y te dejan caer en mitad del vuelo. Descarta 1 mano visible
+y 1 mano oculta', 0,  [TreasureKind::ONEHAND], [TreasureKind::ONEHAND] )
+monsters << Monster.new('Angeles de la noche ibicenca', 14, bad_consequence,
+prize)
+
+# EL GORRÓN EN EL UMBRAL
+prize = Prize.new(3, 1)
+bad_consequence = BadConsequence.new_level_number_of_treasures('Pierdes todos 
+tus tesoros visibles', 0, 20, 0)
+monsters << Monster.new('El gorron en el umbral', 10, bad_consequence, prize)
+
+# H.P. MUNCHCRAFT
+prize = Prize.new(2, 1)
+bad_consequence = BadConsequence.new_level_specific_treasures('Pierdes la 
+armadura visible', 0, [TreasureKind::ARMOR], Array.new)
+monsters << Monster.new('H.P.Munchcraft', 6, bad_consequence, prize)
+
+# BICHGOOTH
+prize = Prize.new(1, 1)
+bad_consequence = BadConsequence.new_level_specific_treasures('Sientes bichos 
+bajo la ropa. Descarta la armadura visible',0, [TreasureKind::ARMOR], Array.new)
+monsters << Monster.new('Bichgooth', 2, bad_consequence, prize)
+
+# EL REY DE ROSA
+prize = Prize.new(4, 2)
+bad_consequence = BadConsequence.new_level_number_of_treasures('Pierdes 5 
+niveles y 3 tesoros visibles', 5, 3, 0)
+monsters << Monster.new('El rey de rosa', 13, bad_consequence, prize)
+
+# LA QUE REDACTA EN LAS TINIEBLAS
+prize = Prize.new(1, 1)
+bad_consequence = BadConsequence.new_level_number_of_treasures('Toses los 
+pulmones y pierdes 2 niveles.', 2, 0, 0)
+monsters << Monster.new('La que redacta en las tinieblas', 2, bad_consequence,
+prize)
 
 # LOS HONDOS
 prize = Prize.new(2, 1)
